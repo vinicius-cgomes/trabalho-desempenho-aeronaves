60c0b81c8b64d91c6d8e120155139dad34837b71
isabelacanellor <isaresener@gmail.com>
2025-04-15 21:27:47 -0300

    Ex 5
    Colocando inputs

--- Ex5.m
+++ Ex5.m
@@ -1,1 +1,22 @@
-% Exercício 5
+% Trabalho Final - Desempenho
+% Parte 4 - Cálculo de alcance e consumo de combustível
+% Exercício 5: Análise de Missão com MTOW Fixo
+
+% Dados do enunciado
+
+% Massas:
+MTOW = 33100; % Peso máximo de decolagem [kg]
+m_f_max = 12700; % Capacidade máxima de combustível [kg]
+OEW = 16400; % Peso operacional vazio [kg]
+
+% Coef. aerodinâmicos:
+CD0 = 0.015;
+k = 0.05;
+CD_noFlap = CD0 + k*CL^2;
+
+% Parâmetros construtivos do avião
+S = 88; % Area da asa [m²]
+
+% Combustível
+ct = 0.7/3600; % Consumo específico TSFC [1/s]
+Tmax = 55600; % Tração máxima [N]
