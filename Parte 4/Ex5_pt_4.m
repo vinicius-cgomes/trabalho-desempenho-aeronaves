8e2a01bc507d9b74c0c3ddaec1d77020bf3381a4
isabelacanellor <isaresener@gmail.com>
2025-05-04 12:54:34 -0300

    parte 4 exercicio 5

--- Parte 4/Ex5_pt_4.m
+++ Parte 4/Ex5_pt_4.m
@@ -6,7 +6,7 @@ close all
 
 % ============ Dados do problema ============
 
-% Modelo da aeronave - talvez tenham infos repetidas de outros códigos
+% Modelo da aeronave
 
 MTOW = 33100; % Peso máximo de decolagem [kg]
 OEW = 16400; % Peso operacional vazio [kg]
@@ -25,14 +25,14 @@ S = 88; % Area da asa [m²]
 
 Hcruise = 10668; % Altitude de cruzeiro [m].
 Havg = Hcruise/2; % Altitude média [m].
-[~,~,~,rhoCruise,~,~] = atmosisa(Hcruise); % Não tenho ctz se é assim ou se pega da questão 1
-[~,~,~,rhoAvg,~,~] = atmosisa(Havg); % Não tenho ctz se é assim ou se pega da questão 1
-[~,~,~,rho0,~,~] = atmosisa(0); % Não tenho ctz se é assim ou se pega da questão 1
-g = 9.8; % Gravidade [m/s²] add mais casas decimais
+[~,~,~,rhoCruise,~,~] = atmosisa(Hcruise); 
+[~,~,~,rhoAvg,~,~] = atmosisa(Havg); 
+[~,~,~,rho0,~,~] = atmosisa(0);
+g = 9.8; % Gravidade [m/s²] 
 
 deltaT = 1; % Posição da manete
 Tmax_SL = 55600; % Tração máxima [N]; AEO; manete = 100%
-Tavg = deltaT * Tmax_SL * (rhoAvg/rho0)^0.6; % Modelo de tração; pelo enunciado, entendi que era pra considerar rhoAvg (mas na prática, rho varia com H)
+Tavg = deltaT * Tmax_SL * (rhoAvg/rho0)^0.6; % Modelo de tração; Considerar rhoAvg (mas na prática, rho varia com H)
 TSFC = 0.7/3600; % Consumo específico TSFC [1/s]
 
 % ============ Estimativas de Subida ============
@@ -66,6 +66,7 @@ for i=1:N
 end
 
 figure(1);
+set(gcf, 'Position', [100 100 800 400]); % [left bottom width height]
 sgtitle('Gráfico de convergência do combustível gasto na subida'); % Figure title
 
 subplot(1,2,1)
@@ -73,14 +74,15 @@ plot(0:N_i, fuelClimb, 'o-')
 grid()
 title('fuel_{Climb}');
 xlabel('Iteração');
-ylabel('fuel_{Climb} [kg]');
+ylabel('fuel_{Climb} (kg)');
 
 subplot(1,2,2)
 plot(0:N_i, difVector, 'o-r')
 grid()
 title('Diferença entre iterações');
 xlabel('Iteração');
-ylabel('fuel_{Climb}^{(i-1)} - fuel_{Climb}^{(i)} [kg]');
+ylabel('fuel_{Climb}^{(i-1)} - fuel_{Climb}^{(i)} (kg)');
+exportgraphics(figure(1), 'fuelClimb.pdf')
 
 % Calcular a distância de subida na direção x com timeClimb e V_climb convergida
 xClimb = vClimb*cos(gamaClimb)*timeClimb; % [m]
@@ -88,7 +90,8 @@ xClimb = vClimb*cos(gamaClimb)*timeClimb; % [m]
 % ============ Estimativas de Descida ============
 
 gamaDesc = deg2rad(-3); % [rad]
-xDesc = Hcruise/abs(tan(gamaDesc)); % valor parece alto, será que tá certo? [m]
+xDesc = Hcruise/abs(tan(gamaDesc)); % valor parece alto [m]
+xDesc_NM = xDesc/1852;
 % Assumir consumo de combustível fixo para a descida
 fuelDesc = 150; % [kg]
 
@@ -137,13 +140,15 @@ vCruiseAvg = [0, 0, 0]; % Velocidade média de cruzeiro [m/s]
 fuelCruise = [0, 0, 0]; % Combustível gasto em cruzeiro [kg]
 
 color = ["b", "r", "g"];
+legendPosition = [0.4, 0.15, 0.1, 0.1]; % [x, y, width, height]
 
+% figure(2)
 for i=1:3
     % Criar os vetores de tempo, velocidade e deslocamento
     t = 0; % Tempo no início do cruzeiro 
     vCruise = sqrt((startCruise*g)/(0.5*rhoAvg*S*CL_Emax)); % Velocidade no início do cruzeiro
     xCruiseVector = 0; % Deslocamento no início do cruzeiro. Será completado de acordo com i no loop abaixo
-    timeStep = 1*60; % Time step; a cada min recalcula a velocidade de cruzeiro
+    timeStep = 1*60; % Time step; a cada minuto recalcula a velocidade de cruzeiro
     
     n = 0;
     xCruise_n = 0; % Inicializar a variável
@@ -168,24 +173,29 @@ for i=1:3
 
     vCruiseAvg(i) = mean(vCruise(2:end)); % Velocidade média de cruzeiro
 
-    figure(i+1)
+    subplot(3,1,i)
     plot(t/(60*60),vCruise, "color",color(i), 'linewidth',1)
     hold on
     plot(t/(60*60),repmat(vCruiseAvg(i), size(t)), "color","k", 'linewidth',1)
     grid()
-    xlabel('Tempo [h]');
-    ylabel('V_{Cruise} [m/s]');
+    xlabel('Tempo (h)');
+    ylabel('V_{Cruise} (m/s)');
     title(strcat('Velocidade de cruzeiro para a missão de ', " ", num2str(xTotal_NM(i)), " NM"));
-    legend('Velocidade de em função do tempo', 'Velocidade média', 'Location', 'best');
+    legend('Velocidade de em função do tempo', 'Velocidade média', 'Location', 'south');
+    ylim([130 190])
+    exportgraphics(figure(i+1), strcat('vCruise_',num2str(xTotal_NM(i)), "NM.pdf"));
 
-    % % Calcular o combustível de cruzeiro. No relatório, comentar a
-    % % simplificação adotada: estamos considerando a velocidade média de
-    % % cruzeiro para calcular o combustível gasto.
+    % Calcular o combustível de cruzeiro.
+    % Simplificação adotada: estamos considerando a velocidade média de
+    % cruzeiro para calcular o combustível gasto.
 
-    W_endCruise = W_startCruise * exp(-xCruise(i)*TSFC/(vCruiseAvg(i)*CLCDmax)); % Peso do avião no final do cuzeiro [N]
+    W_endCruise = W_startCruise * exp(-xCruise(i)*TSFC/(vCruiseAvg(i)*CLCDmax)); % Peso do avião no final do cruzeiro [N]
     fuelCruise(i) = (W_startCruise - W_endCruise)/g; % Massa de combustível gasta em cruzeiro, em média
 
 end
+
+% set(gcf, 'Position', [0 0 900 800]) % posx, posy, Width, Height
+% exportgraphics(gcf, 'combined_vCruise_plots.pdf');
 
 % Cálculo do combustível total e payload
 
@@ -225,9 +235,44 @@ end
 % Análise de consumo
 % Calcular o percentual de combustível gasto em cada etapa
 
-fuelTaxiOut_pct = fuelTaxiOut./fuelTotal
-fuelClimb_pct = fuelClimb(end)./fuelTotal
-fuelCruise_pct = fuelCruise./fuelTotal
-fuelDesc_pct = fuelDesc./fuelTotal
-fuelTaxiLnd_pct = (fuelTaxi - fuelTaxiOut)./fuelTotal
-fuelReserva_pct = fuelReserva./fuelTotal
+fuelTaxiOut_pct = fuelTaxiOut./fuelTotal;
+fuelClimb_pct = fuelClimb(end)./fuelTotal;
+fuelCruise_pct = fuelCruise./fuelTotal;
+fuelDesc_pct = fuelDesc./fuelTotal;
+fuelTaxiLnd_pct = (fuelTaxi - fuelTaxiOut)./fuelTotal;
+fuelReserva_pct = fuelReserva./fuelTotal;
+
+%% Gráficos finais
+
+figure(4);
+
+plot(xTotal_NM, fuelTotal, "o-", "color", color(1), "linewidth",1)
+hold on
+plot(xTotal_NM, payload, "o-", "color", color(2), "linewidth",1)
+grid()
+legend('Combustível Total Requerido', 'Payload', 'Location', 'best');
+title('Comparação entre combustível requerido e payload');
+xlabel('Distância Total da Missão (NM)');
+ylabel('Massa (kg)');
+exportgraphics(figure(4), 'pesos.pdf')
+
+%% Gráfico comparando o precentual gasto em cada etapa por cada missão
+
+fuelData = [fuelTaxiOut_pct; fuelClimb_pct; fuelCruise_pct; fuelDesc_pct; fuelTaxiLnd_pct; fuelReserva_pct];
+x=1:6;
+
+figure(5);
+h = bar(x,[100*fuelData(:,1),100*fuelData(:,2),100*fuelData(:,3)]);
+set(h(1),'facecolor',color(1))
+set(h(2),'facecolor',color(2))
+set(h(3),'facecolor',color(3))
+title('Percentual de combustível gasto por etapa');
+xlabel('Etapa de Voo');
+ylabel('Percentual de combustível consumido');
+grid()
+
+xticklabels({'Táxi Decolagem', 'Subida', 'Cruzeiro', 'Descida', 'Táxi Pouso', 'Reserva'});
+xticks(1:6);  % Posicionalmento dos labels
+
+legend('500 NM', '1500 NM', '3000 NM', 'Location', 'best');
+exportgraphics(figure(5), 'percentualCombGasto.pdf')
